bea1a151dcc5ca145704904c4e419a8c820dffda
Orodje <akpotororodje@gmail.com>
2024-09-04 00:40:31 +0100

    next.config modify

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
-import { withNextVideo } from "next-video/process";
+// import { withNextVideo } from "next-video/process";
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default withNextVideo(nextConfig);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
